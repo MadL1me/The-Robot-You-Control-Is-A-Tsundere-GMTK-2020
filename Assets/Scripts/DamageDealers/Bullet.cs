54b46d59d76121639e61329361ecbbb3bfd5a4c6
mynndesu <mynnme@gmail.com>
2020-07-11 01:09:05 +0200

    Weapon progress

--- Assets/Scripts/DamageDealers/Bullet.cs
+++ Assets/Scripts/DamageDealers/Bullet.cs
@@ -6,11 +6,25 @@ namespace GMTK2020
     { 
         int GetDamage { get; }
     }
+
+    public enum ProjectileSide
+    {
+        Friend,
+        Opponent
+    }
     
     [RequireComponent(typeof(Collider2D))]
     public class Bullet : MonoBehaviour, IDamageDealer
     {
         public int GetDamage => _damage;
         [SerializeField] protected int _damage;
+
+        public float Speed;
+        public ProjectileSide Side;
+
+        private void Update()
+        {
+            
+        }
     }
 }
---
+++ Assets/Scripts/Weapons/WeaponBearer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBearer : MonoBehaviour
+{
+    public Weapon CurrentWeapon;
+    public int CurrentRounds;
+
+    public bool IsReloading { get; private set; }
+
+    private float _reloadStart;
+
+    public void Update()
+    {
+        
+    }
+}
