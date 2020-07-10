5818171d3ceef94f217b78e6ff9f5ebb4c35ada6
Ilushkins <sn0wy-0wl@yandex.ru>
2020-07-11 02:37:56 +0500

    Basic player movement

--- Assets/Scripts/Actors/PlayerMovement.cs
+++ Assets/Scripts/Actors/PlayerMovement.cs
@@ -6,10 +6,26 @@ namespace GMTK2020
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D _rigidbody;
-
+        [SerializeField] private float _movingSpeed;
+        
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            HandleInputs();
+        }
+
+        private void HandleInputs()
+        {
+            var horizontal = Input.GetAxisRaw("Horizontal");
+            var vertical = Input.GetAxisRaw("Vertical");
+
+            var moveVector = new Vector3(horizontal, vertical).normalized * _movingSpeed * Time.deltaTime;
+            
+            transform.Translate(moveVector);
         }
     }
 }
