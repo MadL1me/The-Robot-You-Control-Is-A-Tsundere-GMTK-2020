a4885a28a9b13ac47f5080b61fca15aa182b120c
Ilushkins <sn0wy-0wl@yandex.ru>
2020-07-11 03:35:32 +0500

    Level manager class logic

---
+++ Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GMTK2020.Level
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private List<Enemy> _levelEnemies;
+        
+        private int _totalEnemiesCount;
+        private int _enemiesKilled;
+
+        [SerializeField] private float _delayBetweenRestartAndLevelSwitch;
+        
+        private void Start()
+        {
+            _enemiesKilled = 0;
+            
+            FindEnemies();
+            FindPlayer();
+            
+            ScanLevelField();
+        }
+
+        private void ScanLevelField()
+        {
+            
+        }
+
+        private void FindPlayer()
+        {
+            var player = FindObjectOfType<Player>();
+            player.OnActorDeath += RestartLevel;
+        }
+
+        private void FindEnemies()
+        {
+            _levelEnemies = FindObjectsOfType<Enemy>().ToList();
+            _totalEnemiesCount = _levelEnemies.Count;
+
+            foreach (var enemy in _levelEnemies)
+            {
+                enemy.OnActorDeath += OnEnemyDie;
+            }
+        }
+        
+        public void OnEnemyDie()
+        {
+            _enemiesKilled++;
+            if (_enemiesKilled >= _totalEnemiesCount)
+                ProceedToNextLevel();
+        }
+
+        public void ProceedToNextLevel()
+        {
+            //TODO some cool looking coroutine
+        }
+
+        public void RestartLevel()
+        {
+            //TODO some cool loolking restart
+        }
+    }
+}
