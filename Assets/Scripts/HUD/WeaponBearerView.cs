63eeb00ab0fde7b4e8d27e9a8e4cffb872a2a231
Ilushkins <sn0wy-0wl@yandex.ru>
2020-07-11 17:03:56 +0500

    Multiple weapons and Weapons view update

---
+++ Assets/Scripts/HUD/WeaponBearerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GMTK2020.HUD
+{
+    public class WeaponBearerView : MonoBehaviour
+    {
+        [SerializeField] private Image _weaponImage;
+        [SerializeField] private Text _weaponName;
+        
+        public void SwitchWeapon(WeaponConfig weaponConfig)
+        {
+            _weaponName.text = weaponConfig.WeaponName;
+            _weaponImage.sprite = weaponConfig.WeaponSprite;
+        }
+    }
+}
