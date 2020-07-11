4d84479224ccb1092fdae02f11fc918978d82384
Ilushkins <sn0wy-0wl@yandex.ru>
2020-07-11 18:06:28 +0500

    Basic robot - dasher

---
+++ Assets/Scripts/Actors/Enemies/RobotDasher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using Unity.Collections;
+using UnityEngine;
+
+namespace GMTK2020
+{
+    public class RobotDasher : Enemy
+    {
+        [SerializeField][ReadOnly] private bool _isDashing;
+
+        [SerializeField] private float _prepareTime;
+        [SerializeField] private float _beforeDashDistance;
+        [SerializeField] private float _dashingSpeed;
+        [SerializeField] private float _dashingLength;
+        
+        protected override void Attack() => StartCoroutine(Charge());
+        
+        protected IEnumerator Charge()
+        {
+            _isDashing = true;
+            
+            var distance = _dashingLength * _dashingSpeed;
+            var wait = new WaitForFixedUpdate();
+            
+            yield return new WaitForSeconds(_prepareTime);
+            
+            var direction = (_player.transform.position - transform.position).normalized;
+            
+            for (float i = 0; i < distance; i += _dashingSpeed)
+            {
+                _rigidbody.velocity = direction * _dashingSpeed * Time.fixedDeltaTime;
+                yield return wait;
+            }
+            
+            _isDashing = false;
+        }
+
+        protected override void Move()
+        {
+            _rigidbody.velocity = (_directionToCurrentWaypoint.normalized * _movingSpeed * Time.fixedDeltaTime);
+        }
+
+        protected override void MakeAIDecision()
+        {
+            if (Vector3.Distance(transform.position, _player.transform.position) <= _beforeDashDistance
+            && !_isDashing)
+                Attack();
+            else if (!_isDashing)
+                Move();
+        }
+    }
+}
