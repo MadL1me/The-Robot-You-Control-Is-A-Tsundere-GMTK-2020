93efad78b5a918ddffeea35df03d95839e21fda7
mynndesu <mynnme@gmail.com>
2020-07-12 15:02:27 +0200

    Menu

---
+++ Assets/Scripts/Menu/MenuBlinker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBlinker : MonoBehaviour
+{
+    private Text _text;
+    private float _lastBlink;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (Time.time - _lastBlink > 0.5F)
+        {
+            _lastBlink = Time.time;
+            _text.enabled = !_text.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuParallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuParallax : MonoBehaviour
+{
+    public float ParallaxStrength = 0F;
+
+    private Vector3 _initialPos;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _initialPos = transform.position;
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        var normalPos = new Vector3(Mathf.Clamp(Input.mousePosition.x / (float)_camera.pixelWidth, 0F, 1F), Mathf.Clamp(Input.mousePosition.y / (float)_camera.pixelHeight, 0F, 1F));
+
+        transform.position = _initialPos + -(normalPos - new Vector3(0.5F, 0.5F)) * ParallaxStrength * 100;
+    }
+}
